d6a6d4e6209bbb8bcf2708ba8aef65c8ce72efb0
gintaras68 <gintaras5783@yahoo.com>
2024-01-31 19:58:10 +0200

    Su JavaScript realizavau lapo formavimą ir paišymą

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-columns = 10;
-rows = 10;
+columns = 70;
+rows = 50;
 
 // generuojame lentelę
 table = document.querySelector('tbody');
@@ -12,3 +12,14 @@ for (let i = 0; i < rows; i++) {
     innerCode += "<tr>" + cells + "</tr>"
 }
 table.innerHTML = innerCode;
+
+// kiekvienam <td> elementui - įvykių stebėtoją
+tableBody = document.querySelector('tbody');
+cells = tableBody.querySelectorAll('td');
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseenter', function (event) {
+        if (event.buttons == 1) { event.target.classList.add('pointed'); }
+        if (event.buttons == 2) { event.target.classList.remove('pointed'); }
+    });
+});
