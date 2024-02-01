f3c3b61eede6c922f1264589de3588da4d82553a
gintaras68 <gintaras5783@yahoo.com>
2024-02-01 22:05:11 +0200

    Pridėjau lapo dydžio keitimo galimybę. Kiti pataisymai.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,59 @@
-columns = 70;
-rows = 50;
+defaultColumns = 180;
+defaultRows = 80;
 
-// generuojame lentelę
+controlBlock = document.querySelector('.control');
+columnsInput = controlBlock.querySelector('.inputColumns');
+rowsInput = controlBlock.querySelector('.inputRows');
+button = controlBlock.querySelector('button');
+colValue = document.getElementById('colOutput');
+rowValue = document.getElementById('rowOutput');
 table = document.querySelector('tbody');
-innerCode = '';
-for (let i = 0; i < rows; i++) {
-    cells = '';
-    for (let j = 0; j < columns; j++) {
-        cells += "<td></td>";
-    }
-    innerCode += "<tr>" + cells + "</tr>"
-}
-table.innerHTML = innerCode;
 
-// kiekvienam <td> elementui - įvykių stebėtoją
-tableBody = document.querySelector('tbody');
-cells = tableBody.querySelectorAll('td');
+console.log(columnsInput.value);
+console.log(rowsInput.value);
+console.log(button);
 
-cells.forEach(cell => {
-    cell.addEventListener('mouseenter', function (event) {
-        if (event.buttons == 1) { event.target.classList.add('pointed'); }
-        if (event.buttons == 2) { event.target.classList.remove('pointed'); }
-    });
+// priskliriam reikšmes pagal nutylėjimą valdymo įrankiams
+columnsInput.value = defaultColumns;
+colValue.innerHTML = defaultColumns;
+rowsInput.value = defaultRows;
+rowValue.innerHTML = defaultRows;
+
+
+createTable(defaultColumns, defaultRows);
+
+button.addEventListener('click', (event) => {
+    console.log("Paspausta");
+    event.preventDefault();
+    columns = columnsInput.value;
+    rows = rowsInput.value;
+    // button.setAttribute('disabled', 'true');
+
+    createTable(columns, rows);
 });
+
+
+
+
+// formuojama lentelė ir kiekvienam elementui - įvykių stebėtoją
+function createTable(columns, rows) {
+    innerCode = '';
+    for (let i = 0; i < rows; i++) {
+        cells = '';
+        for (let j = 0; j < columns; j++) {
+            cells += "<td></td>";
+        }
+        innerCode += "<tr>" + cells + "</tr>"
+    }
+    table.innerHTML = innerCode;
+    
+    // kiekvienam <td> elementui - įvykių stebėtoją
+    tableBody = document.querySelector('tbody');
+    cells = tableBody.querySelectorAll('td');
+    cells.forEach(cell => {
+        cell.addEventListener('mousemove', function (event) {
+            if (event.buttons == 1) { event.target.classList.add('pointed'); }
+            if (event.buttons == 2) { event.target.classList.remove('pointed'); }
+        });
+    });
+}
